db143190aba703583d68737fe4b02c882b522b33
Juraj Brilla <brillaju@gmail.com>
2023-04-29 22:46:19 +0200

    Interface filled

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/product/logic/IProductService.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/product/logic/IProductService.java
@@ -1,4 +1,26 @@
 package sk.stuba.fei.uim.oop.assignment3.product.logic;
 
+import javassist.NotFoundException;
+import sk.stuba.fei.uim.oop.assignment3.product.data.Product;
+import sk.stuba.fei.uim.oop.assignment3.product.web.bodies.ProductEditRequest;
+import sk.stuba.fei.uim.oop.assignment3.product.web.bodies.ProductRequest;
+
+import java.util.List;
+
 public interface IProductService {
+
+    List<Product> getAllProducts();
+
+    Product createProduct(ProductRequest productRequest);
+
+    Product getProductById(long id) throws NotFoundException;
+
+    Product updateProduct(long id, ProductEditRequest productEditRequest) throws NotFoundException;
+
+    void deleteProduct(long id) throws NotFoundException;
+
+    int getProductAmount(long id) throws NotFoundException;
+
+    int addProductAmount(long id, int amountPar) throws NotFoundException;
+
 }
